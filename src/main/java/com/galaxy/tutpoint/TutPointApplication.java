dfe549cd8f10199130a284981ecec4f587e8ff59
spartan <spartan@spartan-Lenovo-ideapad-300-15ISK>
2021-12-04 02:32:51 +0530

    initial commit.

---
+++ src/main/java/com/galaxy/tutpoint/TutPointApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.tutpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxy/tutpoint/TutPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.tutpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
